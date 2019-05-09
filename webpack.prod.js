44724c6fe80121605440ba5c10579c4400e643c7
wang tai yao <759743719@qq.com>
2019-05-09 15:43:43 +0800

    修改配置文件，本地运行不清空dist

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
  const merge = require('webpack-merge');
  const common = require('./webpack.common.js');
-
+ const CleanWebpackPlugin = require('clean-webpack-plugin');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const VueLoaderPlugin = require('vue-loader/lib/plugin');
  module.exports = merge(common, {
-   mode: 'production',
+     mode: 'production',
+     plugins: [
+         new CleanWebpackPlugin(), //清空dist
+         new HtmlWebpackPlugin({ template: './src/public/index.html' }), //加载首页,生成index.html
+         new VueLoaderPlugin() //vue 文件加载器最新用法
+     ],
  });
